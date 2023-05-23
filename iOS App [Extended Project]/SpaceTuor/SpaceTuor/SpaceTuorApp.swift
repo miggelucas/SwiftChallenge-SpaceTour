2244ac52aaf8f7884d972d5b1e66fcf922d3d298
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-05-22 21:41:18 -0300

    creating directory for extended project

---
+++ iOS App [Extended Project]/SpaceTuor/SpaceTuor/SpaceTuorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceTuorApp.swift
+//  SpaceTuor
+//
+//  Created by Lucas Migge de Barros on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SpaceTuorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
